c3bb499cb8d9ceb20fb89ebe02d1da3261ccae29
Yotic <55879406+Yoticc@users.noreply.github.com>
2024-04-01 01:31:33 +0300

    Delete MemDec.cs

--- MemDec.cs
+++
@@ -1,6 +0,0 @@
-public class MemDec
-{
-   public bool[] Value { get; set }
-   public MemDec(in int value) => Value = Array.Reverse(new BitArray(value).Cast<bool>());
-   publig decimal GetValue() => Convert.ToInt64(new string(Value.Select(x => x ? '1' : '0')), 2);
-}
